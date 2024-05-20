665c6151d4159e002bc18fcb13608fc105aeef42
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-05-20 23:26:25 +1000

    Omit hidden item names in "Open terminal" context

--- src/Patches/ItemNameInTerminalContext.cs
+++ src/Patches/ItemNameInTerminalContext.cs
@@ -10,15 +10,16 @@ namespace LootTip
         {
             if (__instance.displayNameToken == "MULTISHOP_TERMINAL_NAME") {
                 RoR2.ShopTerminalBehavior terminal = __instance.GetComponent<RoR2.ShopTerminalBehavior>();
-                if (terminal != null) {
-                    // Item display name logic from: RoR2.UI.PingIndicator.RebuildPing()
-                    string item = (terminal.pickupIndexIsHidden || !terminal.pickupDisplay) ? "?" : RoR2.Language.GetString(RoR2.PickupCatalog.GetPickupDef(terminal.CurrentPickupIndex()).nameToken ?? RoR2.PickupCatalog.invalidPickupToken);
-
-                    System.Text.StringBuilder sb = new System.Text.StringBuilder(__result);
-                    sb.Append(" <nobr><style=cStack><i>");
-                    sb.Append(item);
-                    sb.Append("</i></style></nobr>");
-                    __result = sb.ToString();
+                // Item name display checks from: RoR2.UI.PingIndicator.RebuildPing()
+                if (terminal != null && !terminal.pickupIndexIsHidden && terminal.pickupDisplay) {
+                    string item = RoR2.Language.GetString(RoR2.PickupCatalog.GetPickupDef(terminal.CurrentPickupIndex()).nameToken);
+                    if (!string.IsNullOrWhiteSpace(item)) {
+                        System.Text.StringBuilder sb = new System.Text.StringBuilder(__result);
+                        sb.Append("\n  <nobr><style=cStack>");
+                        sb.Append(item);
+                        sb.Append("</style></nobr>");
+                        __result = sb.ToString();
+                    }
                 }
             }
         }
