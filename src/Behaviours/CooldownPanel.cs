4b61e7a9ac40f9ee20ee20d6a44b67fea0723d0e
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-10-14 20:06:41 +1100

    Try create EquipmentCooldownVisual

---
+++ src/Behaviours/CooldownPanel.cs
@@ -0,0 +1,34 @@
+﻿using RoR2;
+using RoR2.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HUDdleUP.Behaviours
+{
+    internal class CooldownPanel : MonoBehaviour
+    {
+        private EquipmentIcon parent;
+        private RawImage cooldownRemapPanel;
+
+        internal static void Init(EquipmentIcon parent, GameObject target)
+        {
+            GameObject clone = Instantiate(target, parent.transform);
+            CooldownPanel instance = clone.AddComponent<CooldownPanel>();
+            instance.parent = parent;
+            instance.cooldownRemapPanel = clone.GetComponent<RawImage>();
+        }
+
+        private void Update()
+        {
+            float alpha = 1;
+            if (parent.hasEquipment) {
+                float total = parent.targetEquipmentSlot._rechargeTime.t;
+                if (total >= Mathf.Epsilon) {
+                    alpha = 1 - parent.targetEquipmentSlot.cooldownTimer / total;
+                }
+            }
+            cooldownRemapPanel.enabled = alpha < 1;
+            cooldownRemapPanel.color = new Color(1, 1, 1, alpha);
+        }
+    }
+}
---
+++ src/Patches/EquipmentCooldownVisual.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RoR2.UI;
+using UnityEngine;
+
+namespace HUDdleUP.Patches
+{
+    [HarmonyPatch]
+    internal static class EquipmentCooldownVisual
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(HUD), nameof(HUD.Awake))]
+        private static void HUD_Awake(HUD __instance)
+        {
+            // Icon accessing logic based on RoR.UI.HUD.Update()
+            if (__instance.skillIcons.Length <= 0 || !__instance.skillIcons[0]) return;
+
+            GameObject toClone = __instance.skillIcons[0].cooldownRemapPanel.gameObject;
+            foreach (EquipmentIcon icon in __instance.equipmentIcons) {
+                Behaviours.CooldownPanel.Init(icon, toClone);
+            }
+        }
+    }
+}
